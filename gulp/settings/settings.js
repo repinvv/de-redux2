c4d66b543a04b579a7af62040db7d47652aa12e9
repinvv <repin.vladv@gmail.com>
2017-12-19 22:19:09 +0300

    minor settings formatting

--- gulp/settings/settings.js
+++ gulp/settings/settings.js
@@ -1,11 +1,13 @@
 const buildPath = './build';
 
+const src = 'src';
+const testSrc = 'testSrc';
 const settings = {
-  srcPath: './src',
-  testPath: './testSrc',
+  srcPath: `./${src}`,
+  testPath: `./${testSrc}`,
   buildPath,
-  srcBuildPath: buildPath + '/src',
-  testBuildPath: buildPath + '/testSrc',
+  srcBuildPath: `${buildPath}/${src}`,
+  testBuildPath: `${buildPath}/${testSrc}`
 }
 
 module.exports = settings;
