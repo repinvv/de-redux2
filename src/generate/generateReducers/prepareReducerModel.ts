eee7dd4d55fe161ff84d875fb47bfb315def8055
repinvv <repin.vladv@gmail.com>
2018-10-29 11:14:56 +0300

    tried to resume

--- src/generate/generateReducers/prepareReducerModel.ts
+++ src/generate/generateReducers/prepareReducerModel.ts
@@ -1,16 +1,17 @@
 import { groupBy } from "@vlr/map-tools";
 import { NodeId, Tree } from "../../createTree/types";
 import { PrepReduction } from "../../prepareFiles/types";
+import { createReducerName } from "../helpers/createReducerName";
+import { createReducerPath, getReducerFolder } from "../helpers/createReducerPath";
+import { getNode } from "../helpers/getNode";
+import { getStateName } from "../helpers/getStateName";
+import { createDirectActionsFileImport } from "./createDirectActionsFileImport";
 import { createPaths } from "./createPaths/createPaths";
 import { ModelPath } from "./createPaths/types";
 import { createReducerImports } from "./createReducerImports";
 import { createReductionBlocks } from "./createReductionBlocks";
 import { filterReductions } from "./filterReductions";
 import { ReducerGenModel } from "./types";
-import { getReducerFolder, createReducerPath } from "../helpers/createReducerPath";
-import { getNode } from "../helpers/getNode";
-import { createReducerName } from "../helpers/createReducerName";
-import { createDirectActionsFileImport } from "./createDirectActionsFileImport";
 
 export function prepareReducerModels(reductions: PrepReduction[], tree: Tree): ReducerGenModel[] {
   const paths = groupBy(createPaths(tree), path => path.root);
@@ -27,6 +28,7 @@ function prepareReducerModel(root: NodeId, paths: ModelPath[], reductions: PrepR
     imports: createReducerImports(folder, blocks),
     reducerName: createReducerName(stateId),
     actionsFile: createDirectActionsFileImport(stateId),
+    rootStateType: getStateName(stateId).
   };
 }
 
